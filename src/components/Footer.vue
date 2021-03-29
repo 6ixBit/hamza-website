989c6e85a7d9c9f189234e3fd6a2504554dcc9ef
Hamza Carew <hamzacarew@gmail.com>
2021-03-29 15:01:12 -0400

    Added responsiveness to footer on mobile devices with low width

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,34 +1,40 @@
-<template lang="">
-   <footer>
+<template>
+  <footer>
     <div class="footer">
       <p>Copyright 1888 ©</p>
 
       <ul class="icons">
-        <img src="/github.svg" alt="github_icon">
-        <img src="/linkedin.svg" alt="linkedin_icon">
-        <img src="/email.svg" alt="email_icon">
+        <a href="https://github.com/6ixBit">
+          <img src="/github.svg" alt="github_icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/hamza-carew-26b05b159/">
+          <img src="/linkedin.svg" alt="linkedin_icon" />
+        </a>
+        <a href="mailto:h@gmail.com">
+          <img src="/email.svg" alt="email_icon" />
+        </a>
       </ul>
+
     </div>
   </footer>
 </template>
 
 <script>
 export default {
-    name: "Footer"
-}
+  name: "Footer",
+};
 </script>
 
-<style scoped>   
-
+<style scoped>
 .footer {
-  @apply flex justify-between items-center bg-black text-white px-16 md:px-28 bottom-0 left-0 w-full h-16 z-0;
+  @apply flex flex-col md:flex-row justify-between items-center bg-black text-white px-16 md:px-28 w-full h-20;
 }
 
 p {
-    @apply text-lg font-bold;
+  @apply text-base md:text-lg font-bold;
 }
 
 .icons {
- @apply flex flex-row space-x-4 z-10 fill-current text-white;
+  @apply flex flex-row pt-2 order-first md:order-last space-x-8 md:space-x-6 z-10 text-white;
 }
 </style>
