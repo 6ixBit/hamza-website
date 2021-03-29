9a9a420f33a832c98d1606bef04dd86a85978452
Hamza Carew <hamzacarew@gmail.com>
2021-03-29 14:13:49 -0400

    Refactored entire home page into separate components

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,57 +1,17 @@
 <template>
   <NavBar />
-
-  <div class="general_section">
-    <div class="description">
-      <h2 class="description_title">
-        Web Developer, Tech Enthusiast, and Book Worm.
-      </h2>
-
-      <p class="description_text">
-        When an unknown printer took a galley of type and scrambled it to make a
-        type specimen book.
-      </p>
-
-      <div class="description_buttons">
-        <a href="mailto:h@gmail.com" class="emailButton">E-mail</a>
-        <a class="cvButton" href="/HamzaCV.pdf">Download CV</a>
-        <!-- push to #contact -->
-      </div>
-    </div>
-
-    <img class="inner_img" src="/hamza.jpeg" alt="Icon of hamza" />
-  </div>
-
-  <div class="about_section" id="about">
-    <h2 class="about_title">About</h2>
-
-    <p class="about_text">
-      Lorem Ipsum has been the industry's standard dummy text ever since the
-      1500s, when an unknown printer took a galley of type and scrambled it to
-      make a type specimen books.
-    </p>
-  </div>
-
-  <div class="projects_section" id="projects">
-    <h2 class="projects_title">Projects portal</h2>
-
-    <p class="projects_intro">
-      Lorem Ipsum has been the industry's standard dummy text ever since the
-      1500s, when an unknown printer took a galley of type and scrambled it to
-      make a type specimen book.
-    </p>
-
-   
-
-    <!-- Add container of cards, each card represents a project currently up. -->
-  </div>
-
+  <General_section />
+  <About />
+  <Projects />
   <Footer />
 </template>
 
 <script setup>
 import NavBar from "./NavBar.vue";
 import Footer from "./Footer.vue";
+import Projects from "./Projects.vue";
+import About from "./About.vue";
+import General_section from "./General_section.vue";
 </script>
 
 <style scoped>
@@ -65,65 +25,4 @@ import Footer from "./Footer.vue";
 html {
   scroll-behavior: smooth;
 }
-
-.inner_img {
-  @apply rounded-full lg:h-56 lg:w-56 h-28 w-28 md:h-48 md:w-48 mt-10 md:mt-6 md:mr-24 order-first md:order-last lg:mr-32;
-}
-
-.emailButton {
-  @apply text-white bg-black rounded-md font-bold py-2 px-2 mx-4;
-}
-
-.cvButton {
-  @apply text-black bg-white rounded-md py-2 px-2 border-2 border-black;
-}
-
-.image_container {
-  @apply mt-6 mr-8;
-}
-
-.general_section {
-  @apply flex items-center md:flex-row flex-col justify-center md:justify-around text-gray-400 h-96 text-black;
-}
-
-.description {
-  @apply mt-4 md:mt-10 w-72;
-}
-
-.description_title {
-  @apply mb-4 font-black text-2xl text-black;
-}
-
-.description_buttons {
-  @apply mt-6;
-}
-
-.description_text {
-  @apply text-black text-left;
-}
-
-.about_section {
-  @apply bg-black h-96 md:h-80  text-white py-6 mt-10;
-}
-
-.about_title {
-  @apply font-mono font-bold text-2xl;
-}
-
-.about_text {
-  @apply font-mono break-words text-lg py-4 px-6 lg:px-72 text-center;
-}
-
-.projects_section {
-  @apply h-96 md:h-80 py-6;
-}
-
-.projects_title {
-  @apply font-mono text-2xl font-bold;
-}
-
-.projects_intro {
-  @apply font-mono break-words text-lg py-4 px-6 lg:px-72;
-}
-
 </style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,36 @@
+<template lang="">
+    <div class="projects_section" id="projects">
+    <h2 class="projects_title">Projects portal</h2>
+
+    <p class="projects_intro">
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book.
+    </p>
+
+   
+
+    <!-- Add container of cards, each card represents a project currently up. -->
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.projects_section {
+  @apply h-96 md:h-80 py-6;
+}
+
+.projects_title {
+  @apply font-mono text-2xl font-bold;
+}
+
+.projects_intro {
+  @apply font-mono break-words text-lg py-4 px-6 lg:px-72;
+}
+</style>
+    
